0ab504972e8bd5d70869959110e316a775626ce6
Fernando <98433286+ferguez22@users.noreply.github.com>
2024-11-17 12:15:47 +0100

    Usuarios listos

--- src/models/usuarios.model.js
+++ src/models/usuarios.model.js
@@ -1,13 +1,46 @@
 const poolSQL = require("../config/db.js");
 
+// select * from usuarios
+const obtenerUsuarios = async () => {
+    const resultado = await poolSQL.query('SELECT * FROM usuarios');
+    return resultado[0];
+}
+
+// select * from usuarios where id = ?
 const seleccionarUsuarioPorId = async (id) => {
     const [resultado] = await poolSQL.query(
-        'select * from usuarios where id = ?', 
+        'SELECT * FROM usuarios WHERE id = ?',
         [id]
     );
     return resultado[0];
 }
 
+// insert into usuarios (nombre, email, contraseña, rol) values (?, ?, ?, ?)
+const insertarUsuario = async ({ nombre, email, contraseña, rol }) => {
+    const [resultado] = await poolSQL.query(
+        'INSERT INTO usuarios (nombre, email, contraseña, rol) VALUES (?, ?, ?, ?)',
+        [nombre, email, contraseña, rol]
+    );
+    console.log(resultado);
+    return resultado;
+}
+
+// update usuarios set nombre=?, email=?, contraseña=?, rol=? where id=?
+const updateUsuarioPorId = (usuarioId, { nombre, email, contraseña, rol }) => {
+    const result = poolSQL.query('UPDATE usuarios SET nombre=?, email=?, contraseña=?, rol=? WHERE id=?', [nombre, email, contraseña, rol, usuarioId]);
+    return result;
+}
+
+// delete from usuarios where id = ?
+const deleteUsuarioPorId = (usuarioId) => {
+    const result = poolSQL.query('DELETE FROM usuarios WHERE id =?', [usuarioId]);
+    return result;
+}
+
 module.exports = {
-    seleccionarUsuarioPorId
+    seleccionarUsuarioPorId,
+    obtenerUsuarios,
+    insertarUsuario,
+    deleteUsuarioPorId,
+    updateUsuarioPorId
 };
--- src/routes/api/usuarios.routes.js
+++ src/routes/api/usuarios.routes.js
@@ -1,7 +1,17 @@
 //URL BASE: /api/usuarios
+
 const express = require('express');
 const enrutador = express.Router();
+const { getUsuarios, registrarUsuario, actualizarUsuario, eliminarUsuario, getUsuarioPorId } = require("../../controllers/usuarios.controller.js");
 
 
+enrutador.get("/", getUsuarios);
+enrutador.get("/:id", getUsuarioPorId);
+
+enrutador.post("/", registrarUsuario);
+
+enrutador.put("/:id", actualizarUsuario);
+
+enrutador.delete("/:id", eliminarUsuario);
 
 module.exports = enrutador;
