9f88980264daf61c7cb11ab2767654b43dc6e3bf
Alvaro <koonery@gmail.com>
2024-11-18 09:33:13 +0100

    fix: rutas de suscripciones comentadas para que no den errores

--- src/routes/api/suscriptores.routes.js
+++ src/routes/api/suscriptores.routes.js
@@ -5,13 +5,13 @@ const enrutador = express.Router();
 
 const { validarExisteEmailSuscriptor } = require('../../utils/middelwares.js');
 
-enrutador.get("/", getUsuarios);
-enrutador.get("/:id", getUsuarioPorId);
+// enrutador.get("/", getUsuarios);
+// enrutador.get("/:id", getUsuarioPorId);
 
-enrutador.post("/", validarExisteEmailSuscriptor, registrarUsuario);
+// enrutador.post("/", validarExisteEmailSuscriptor, registrarUsuario);
 
-enrutador.put("/:id", actualizarUsuario);
+// enrutador.put("/:id", actualizarUsuario);
 
-enrutador.delete("/:id", eliminarUsuario);
+// enrutador.delete("/:id", eliminarUsuario);
 
 module.exports = enrutador;
