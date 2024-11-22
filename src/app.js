8817e95a6da1c47df88c2a20403e25475d246ed6
Alvaro <koonery@gmail.com>
2024-11-22 14:17:02 +0100

    fix: gestión del error max_user_connections para evitar que la api se bloquee

--- src/app.js
+++ src/app.js
@@ -11,8 +11,19 @@ app.use('/api', require('./routes/api.routes'));
 
 // Error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).json(err);
+    console.error(err.stack);
+    // Error max_user_connections
+    if (err.message && err.message.includes('max_user_connections')) {
+        return res.status(503).json({
+            error: 'Servicio no disponible',
+            message: 'Se ha excedido la cantidad de peticiones permitidas. Vuelva a intentarlo más tarde.'
+        });
+    }
+    // Otros errores
+    res.status(500).json({
+        error: 'Error Interno en el servidor',
+        message: err.message
+    });
 })
 
 module.exports = app;
