81f220b08af698855c2d90904e85f49ce02623b3
jaimitro <jaimepavonp@gmail.com>
2024-11-24 13:32:16 +0100

    cambio de diseño de los mail de noticias

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -41,16 +41,18 @@ const enviarEmailNuevaNoticia = async (noticia) => { //TODO añadir enlace para 
             para: datosSuscriptor.email,
             asunto: `Nueva noticia publicada en ${categoria.nombre} en Upgrade Journal.`,
             texto: `Se ha publicado una nueva noticia de la categoría ${categoria.nombre} en Upgrade Journal.`,
-            textohtml: `
-                <div style="background-color: lavender; padding: 5px; margin: 5px; border: 1px solid black; border-radius: 10px;">
-                    <h2 style="padding: 10px; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: darkblue">Upgrade Journal</h2>
-                    <p style="padding: 10px; font-family: Arial; font-size: 18px;">Se ha publicado una nueva noticia de la categoría ${categoria.nombre} en Upgrade Journal.</p>
+            textohtml: `<div style="background-color: white; padding: 5px; margin: 5px; border: 1px solid grey; border-radius: 10px; box-shadow: 0px 5px 5px grey; text-align: center;">
+                <img src="http://localhost:4200/images/upgradejournallogo.png" style="width: 150px">
+                <p style="padding: 10px; font-family: Arial; font-size: 18px;">Se ha publicado una nueva noticia de la categoría ${categoria.nombre} en Upgrade Journal.</p>
+                <div style="background-color: white; padding: 5px; margin: 5px; border: 1px solid grey; border-radius: 10px;">
                     <div style="padding-bottom: 20px;">
-                        <a href="http://localhost:4200/noticias/${categoria.slug}/${noticia.slug}" style="text-decoration: none; color: darkblue; padding: 10px; font-family: Arial; font-size: 18px; font-style: italic">${noticia.titular}</a>
+                        <a href="http://localhost:4200/noticias/${categoria.slug}/${noticia.slug}" style="text-decoration: none; color: black; padding: 10px; font-family: Arial; font-size: 18px; font-style: italic">${noticia.titular}</a>
                     </div>
                     <img src="${noticia.imagen}" alt="${noticia.titular}" style="padding: 10px; padding-bottom: 30px; max-width: 80vw;">
                     <p style="padding: 10px; font-family: Arial; font-size: 12px;">Has recibido este email porque te has dado de alta en la categoría ${categoria.nombre} de Upgrade Journal con el email ${datosSuscriptor.email}</p>
-                </div>`,
+                </div>
+                
+            </div>`,
         };
         enviarEmailSuscriptor(datosEmail);
     }
